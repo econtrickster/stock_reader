41be6a8e65fa496b61eaee44b62e54d77e9aa570
Jia <clarkshiu@gmail.com>
2025-05-18 17:45:26 -0400

    Added comment

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import yfinance as yf
 # Example: Get historical data for Apple
 stock_data = yf.download("AAPL", start="2020-01-01", end="2024-01-01", interval="1d")
 
-
+#Example of loading stock data via yfinance
 print(stock_data)
